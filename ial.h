b1ecefaf0d057f652da6c2a375f4618b5fae0919
jirkacechak <j.cechak1@seznam.cz>
2016-11-12 14:14:31 +0100

    Update ial.h

--- ial.h
+++ ial.h
@@ -2,12 +2,13 @@
 #define _IAL_H
 
 #include <stdio.h>
-#include <malloc.h>
 #include <stdbool.h>
 #include <string.h>
 
 #include "str.h"
 #include "struct.h"
+#include "error.h"
+#include "memory_manager.h"
 
 /*
  * Funkce pro vyhledavani podretezce v retezci
@@ -30,19 +31,32 @@ union ht_value {
 	double value_double;
 	String *value_String;
 };
- 
-typedef struct ht_item {
-	char *key; // nazev
-	token_type type; // "struct.h", u funkce return type
+
+typedef struct ht_params {
+	char *name;
+	token_type type; 
+	struct ht_params *next;
+} *ht_params_ptr;
+
+typedef struct ht_item_func {
+	token_type type; // "struct.h", return type
 	int declared;
 	int defined;
-	int function; // funkce, kde byla deklarovana promena nebo oznaceni
-	union ht_value value; // data promenne
-	struct ht_item *param; // ukazatel na parametr funkce
-} *ht_item_ptr;
+	ht_params_ptr param; // parametry funkce
+	struct hash_table *ht; // tabulka symbolu pro proměnné dané funkce
+} *ht_item_func_ptr;
+
+typedef struct ht_item_var {
+	token_type type; // "struct.h"
+	int defined;
+	String *label; // platnost proměnné
+	union ht_value value; // data proměnné
+} *ht_item_var_ptr;
 
 typedef struct ht_table_item {
-	ht_item_ptr item; // obsah polozky v tabulce
+	String *key; // název
+	ht_item_var_ptr var;
+	ht_item_func_ptr func;
 	struct ht_table_item *next;
 } *ht_table_item_ptr;
 
@@ -54,8 +68,12 @@ typedef struct hash_table {
 unsigned int hash_function(const char *str, unsigned htab_size);
 hash_table_ptr ht_init(unsigned size);
 void ht_free(hash_table_ptr ht);
-void ht_item_clear(ht_item_ptr item);
-bool ht_insert(ht_item_ptr item, hash_table_ptr ht);
-ht_table_item_ptr ht_search(char *key, hash_table_ptr ht);
+void ht_item_func_clear(ht_item_func_ptr item);
+void ht_item_var_clear(ht_item_var_ptr item);
+ht_item_var_ptr ht_create_item_var(token_type type, int defined, String *label, union ht_value value);
+ht_item_func_ptr ht_create_item_func(token_type type, int declared, int defined, ht_params_ptr param, struct hash_table *ht);
+bool ht_insert_item_var(ht_item_var_ptr item, String *key, hash_table_ptr ht);
+bool ht_insert_item_func(ht_item_func_ptr item, String *key, hash_table_ptr ht);
+ht_table_item_ptr ht_search(String *key, bool func, hash_table_ptr ht);
 
 #endif
